16c08ca252dbccd384fd085b03389c2bf9e9cf83
Enock <djsean@Enocks-MBP.lan>
2025-07-30 09:46:34 +0300

    aronia2025

--- tours_travels/urls.py
+++ tours_travels/urls.py
@@ -20,7 +20,6 @@ urlpatterns = [
     path('events/', include(('events.urls', 'events'), namespace='events')),
     path('users/', include(('users.urls', 'users'), namespace='home')),
     path('tours/', include(('adminside.urls', 'adminside'), namespace='adminside')),
-    path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
     path('login/',auth_views.LoginView.as_view(template_name='users/dede/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/dede/index.html'),name='logout'),
     path('mail/',tours_travels_views.mail,name='mail'),
